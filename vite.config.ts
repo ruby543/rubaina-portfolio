7997704821dc9c0c541a7851dbe3f2ba38fb1364
ruby543 <roshanrubaina@gmail.com>
2025-08-12 16:28:41 -0400

    Updated configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/rubaina-portfolio/', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
